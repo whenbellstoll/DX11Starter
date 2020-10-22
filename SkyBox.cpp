6c475007b197e00a8615598c8410422b85858b5a
Kent Reese <kentreese64@gmail.com>
2020-10-22 01:55:29 -0400

    SkyBox Constructor

--- SkyBox.cpp
+++ SkyBox.cpp
@@ -1,1 +1,35 @@
 #include "SkyBox.h"
+
+SkyBox::SkyBox(Microsoft::WRL::ComPtr<ID3D11SamplerState> sO, Microsoft::WRL::ComPtr<ID3D11ShaderResourceView> srv, Microsoft::WRL::ComPtr<ID3D11Device> device, Mesh* c, SimplePixelShader* sps, SimpleVertexShader* svs)
+{
+	samplerOptions = sO;
+	srvSky = srv;
+	cube = c;
+	ps = sps;
+	vs = svs;
+
+	// Rasterizer initialization
+	D3D11_RASTERIZER_DESC ras = {};
+	ras.FillMode = D3D11_FILL_SOLID;
+	ras.CullMode = D3D11_CULL_FRONT;
+
+	device->CreateRasterizerState(&ras, rasterState.GetAddressOf());
+
+	// Depth Stencil State initialization
+	D3D11_DEPTH_STENCIL_DESC dep = {};
+	dep.DepthEnable = true;
+	dep.DepthFunc = D3D11_COMPARISON_LESS_EQUAL;
+
+	device->CreateDepthStencilState(&dep, stencilState.GetAddressOf());
+
+}
+
+SkyBox::~SkyBox()
+{
+	// initalize all our pointers in Game.cpp, should delete them there.
+}
+
+void SkyBox::Draw()
+{
+
+}
--- SkyBox.h
+++ SkyBox.h
@@ -1,5 +1,23 @@
 #pragma once
+
+#include "DXCore.h"
+#include "SimpleShader.h"
+#include "Mesh.h"
+#include <DirectXMath.h>
+#include <wrl/client.h>
 class SkyBox
 {
+	Microsoft::WRL::ComPtr<ID3D11SamplerState> samplerOptions;
+	Microsoft::WRL::ComPtr<ID3D11ShaderResourceView> srvSky;
+	Microsoft::WRL::ComPtr<ID3D11DepthStencilState> stencilState;
+	Microsoft::WRL::ComPtr<ID3D11RasterizerState> rasterState;
+	Mesh* cube;
+	SimplePixelShader* ps;
+	SimpleVertexShader* vs;
+
+	SkyBox(Microsoft::WRL::ComPtr<ID3D11SamplerState> sO, Microsoft::WRL::ComPtr<ID3D11ShaderResourceView> srv, Microsoft::WRL::ComPtr<ID3D11Device> device, Mesh * c, SimplePixelShader * sps, SimpleVertexShader * svs);
+	~SkyBox();
+
+	void Draw();
 };
 
